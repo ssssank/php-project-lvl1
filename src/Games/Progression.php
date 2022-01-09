6f0b603e148ea765670866d90197d6cf8715c9e7
Alexander <ale.litvinov@gmail.com>
2022-01-09 22:06:26 +0300

    Make step 8

---
+++ src/Games/Progression.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Brain\Games\Progression;
+
+use Brain\Engine;
+
+use function Brain\Utils\getRandomInt;
+
+const RULE = 'What number is missing in the progression?';
+const PROGRESSION_LENGTH = 10;
+
+function getProgression($length, $start, $step)
+{
+    $progression = [];
+    for ($i = 0; $i < $length; $i += 1) {
+        array_push($progression, $start + $step * $i);
+    }
+    return $progression;
+}
+
+function prepareRound()
+{
+    $progressionStart = getRandomInt();
+    $progressionStep = getRandomInt();
+    $progression = getProgression(PROGRESSION_LENGTH, $progressionStart, $progressionStep);
+    $randomIndex = getRandomInt(0, PROGRESSION_LENGTH);
+    $rightAnswer = $progression[$randomIndex];
+    $progression[$randomIndex] = '..';
+    $question = implode(' ', $progression);
+    return [
+        'question' => (string) $question,
+        'answer' => (string) $rightAnswer
+    ];
+}
+
+function startGame()
+{
+    $dataForGames = [];
+    for ($i = 0; $i < Engine\WIN_ANSWERS_NUMBER; $i += 1) {
+        $dataForGames[] = prepareRound();
+    }
+    return Engine\runGame(RULE, $dataForGames);
+}
