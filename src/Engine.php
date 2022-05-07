af7daf23eed25633faeeb55ef9a69f5674560426
ssssank <ssssank@gmail.com>
2022-05-07 23:00:49 +0300

    refactor engine

--- src/Engine.php
+++ src/Engine.php
@@ -8,34 +8,29 @@ use function cli\out;
 
 const WIN_ANSWERS_NUMBER = 3;
 
-function runGame(string $rule, array $dataForGames)
+function runGame(string $rule, array $rounds)
 {
-    $rightAnswerCounter = 0;
-
     line("Welcome to the Brain Game!");
     line($rule);
 
     $userName = prompt("May I have your name?", 'anonymous', ' ');
     line("Hello, %s!", $userName);
 
-    while ($rightAnswerCounter < WIN_ANSWERS_NUMBER) {
-        $questionWithAnswer = $dataForGames[$rightAnswerCounter];
+    for ($i = 0; $i < WIN_ANSWERS_NUMBER; $i += 1) {
+        $questionWithAnswer = $rounds[$i];
         $currentQuestion = $questionWithAnswer['question'];
         $rightAnswer = $questionWithAnswer['answer'];
         line("Question: $currentQuestion");
         $userAnswer = prompt("Your answer");
         if ($userAnswer === $rightAnswer) {
             line("Correct!");
-            $rightAnswerCounter += 1;
         } else {
             out("'%s' is wrong answer ;(. ", $userAnswer);
             line("Correct answer was '%s'.", $rightAnswer);
             line("Let's try again, %s!", $userName);
-            break;
+            return null;
         }
     }
 
-    if ($rightAnswerCounter === WIN_ANSWERS_NUMBER) {
-        line("Congratulations, %s!", $userName);
-    }
+    line("Congratulations, %s!", $userName);
 }
